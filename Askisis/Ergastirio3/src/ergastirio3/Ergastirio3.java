df993e32f9daf8ad5b90b83823ef6a37b72f66ea
panosale <panosalevr@hotmail.gr>
2023-04-23 01:54:29 +0300

    .

--- Askisis/Ergastirio3/src/ergastirio3/Ergastirio3.java
+++ Askisis/Ergastirio3/src/ergastirio3/Ergastirio3.java
@@ -17,12 +17,20 @@ static int fibonacci(int f) {
    else
        return fibonacci(f - 1) + fibonacci(f - 2);
 }
-
-static Boolean palindromos (String s) {
+static int isReversed = 0; // Xrisi global metavlitis gia tin evresi tou palindromou Integer. Prepei na arxikopoieitai kathe fora prin tin klisi tis methodou.
+static int palindromosArithmos(int n) {
+    if (n > 0) {
+        int tmp = n % 10;
+        isReversed = isReversed * 10 + tmp;
+        palindromosArithmos(n / 10);
+    }
+    return isReversed;
+}
+static Boolean palindromoString(String s) {
     if (s.length() == 0 || s.length() == 1)
         return true;
     if (s.charAt(0) == s.charAt(s.length()-1))
-        return palindromos(s.substring(1, s.length()-1));
+        return palindromoString(s.substring(1, s.length()-1));
     return false;
 }
 static boolean protosArithmos(int start, int end) {
@@ -48,16 +56,28 @@ public static void main(String[] args) {
     System.out.println(nPar + "!: " + nParagontiko(nPar) + "\n");
     int fibo = 10;
     System.out.println(fibo + " Fibonacci is: " + fibonacci(fibo) + "\n");
-    String pal = "pasnsap";
-    System.out.println(pal + " is palindromos: " + palindromos(pal) + "\n");
-    pal = "pasnap";
-    System.out.println(pal + " is palindromos: " + palindromos(pal) + "\n");
-    System.out.println("Protoi arithmoi.");
-    int prime = 21;
-    int i;
-    for (i = 1; i <= prime; i++)
-        if (protosArithmos(i, prime))
-            System.out.println("To " + i + " einai protos arithmos.\n");
+    int palInt1 = 87362;
+    isReversed = 0; // Arxikopoiisi tis global metavlitis prin ti xrisi tiw methodou gia tin evresi tou palindromou Integer
+    if (palInt1 == palindromosArithmos(palInt1))
+        System.out.println("O "+ palInt1 + " einai Palindromos.");
+    else
+        System.out.println("O "+ palInt1 + " den einai Palindromos.");
+    int palInt2 = 87378;
+    isReversed = 0; // Arxikopoiisi tis global metavlitis prin ti xrisi tiw methodou gia tin evresi tou palindromou Integer
+    if (palInt2 == palindromosArithmos(palInt2))
+        System.out.println("O "+ palInt2 + " einai Palindromos.");
+    else
+        System.out.println("O "+ palInt2 + " den einai Palindromos.");
+    String palStr = "anna";
+    System.out.println(palStr + " einai palindromo: " + palindromoString(palStr) + "\n");
+    palStr = "Anna";
+    System.out.println(palStr + " einai palindromo: " + palindromoString(palStr) + "\n");
+//    System.out.println("Protoi arithmoi.");
+//    int prime = 21;
+//    int i;
+//    for (i = 1; i <= prime; i++)
+//        if (protosArithmos(i, prime))
+//            System.out.println("To " + i + " einai protos arithmos.\n");
 //        System.out.println("Protoi: " + i + " is protos: " + protosArithmos(i, prime));// + "\n-----");
     }
 }

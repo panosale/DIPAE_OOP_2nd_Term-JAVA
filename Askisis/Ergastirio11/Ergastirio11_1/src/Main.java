fe03e709fa677f8bf160280e776f6d84ebda67e3
panosale <panosalevr@hotmail.gr>
2023-05-30 19:51:19 +0300

    Update Main.java

--- Askisis/Ergastirio11/Ergastirio11_1/src/Main.java
+++ Askisis/Ergastirio11/Ergastirio11_1/src/Main.java
@@ -2,6 +2,11 @@
 // ergastirio 11.1
 public class Main {
     public static void main(String[] args) {
+        short tmp_childNo;
         Employee employee = new Employee();
+        System.out.print("Δώσε τον αριθμό των παιδιών του υπαλλήλου: ");
+        tmp_childNo = UserInput.getShort();
+        if (tmp_childNo <= 0)
+            throw new NonPositiveException
     }
 }

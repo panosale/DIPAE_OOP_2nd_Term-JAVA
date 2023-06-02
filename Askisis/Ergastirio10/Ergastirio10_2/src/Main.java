024b7b82279cef80d0051ca9d69b829fe2ca76c6
panosale <panosalevr@hotmail.gr>
2023-06-02 10:36:01 +0300

    Update Main.java

--- Askisis/Ergastirio10/Ergastirio10_2/src/Main.java
+++ Askisis/Ergastirio10/Ergastirio10_2/src/Main.java
@@ -1,7 +1,98 @@
+import java.util.Random;
+
 // Diasindesis - Pollapli klironomikotita
 // ergastirio 10.2
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 100; // Μέγεθος πινάκων
+        Random rnd = new Random();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int under50 = 0, over50 = 0;
+        for (int i = 0; i < n; i++) {
+            if (rnd.nextInt() < 50)
+                under50++;
+            else
+                over50++;
+        }
+        System.out.println("Under 50 found: " + under50);
+        System.out.println("Over 50 found: " + over50);
+//        Pegasus[] Ranch = new Pegasus[n];
+
     }
 }

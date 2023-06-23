8605a408dfc7647af49fc56108c3fbfc3cf1b8c0
panosale <panosalevr@hotmail.gr>
2023-06-23 18:59:55 +0300

    .

--- PaliaThemata/OOP_E20-21A_K2/src/F.java
+++ PaliaThemata/OOP_E20-21A_K2/src/F.java
@@ -1,2 +1,2 @@
-public class F implements E {
+public class F extends E {
 }
--- PaliaThemata/OOP_E20-21A_K2/src/Main.java
+++ PaliaThemata/OOP_E20-21A_K2/src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static int dispSquares(int n) {
-        System.out.println(n * n);
+        System.out.print(n * n + " ");
         if (n == 1) return 1;
         return dispSquares(n-1);
     } // method dispSquares. Τα εμφανίζει σε ανάποδη σειρά. Θέλει έλεγχο
+    public static void dispSquares2(int n) {
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            dispSquares2(n - 1);
+            System.out.println(n * n);
+        } // method dispSquares2.
+    }
+    public static void dispSquares3(int n) {
+        if (n < 1) {
+            return;
+        }
+        dispSquares3(n - 1);
+        int square = n * n;
+        System.out.print(square + " ");
+    } // method dispSquares3.
     public static void main(String[] args) {
         System.out.println("Hello world!\nΘέματα OOP_E20-21A_K2.");
         // ΘΕΜΑ 1.1.i
@@ -14,5 +30,10 @@ public class Main {
         // ΘΕΜΑ 1.1.ii
         // ΘΕΜΑ 2.2
         dispSquares(5);
+        System.out.println();
+        dispSquares2(5);
+        System.out.println();
+        dispSquares3(5);
+        System.out.println();
     }
 }

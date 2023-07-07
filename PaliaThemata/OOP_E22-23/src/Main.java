662e8da665e145c7e1782dc568896c569a1c9c2a
panosale <62485609+panosale@users.noreply.github.com>
2023-07-07 17:52:46 +0300

    Update Main.java

--- PaliaThemata/OOP_E22-23/src/Main.java
+++ PaliaThemata/OOP_E22-23/src/Main.java
@@ -7,10 +7,16 @@ public class Main {
         cust.AddPayment(1000);
 */
         for (int i = 0; i < 10; i++) { // Καταχώρηση 10 πληρωμών. Ζητούμενο της άσκησης.
-            System.out.print("Δώσε το ποσό πληρωμής[" + i + "] : ");
-            cust.AddPayment(UserInput.getDouble()); // Την εισαγωγή του χρήστη απ' το πληκτρολόγιο τη δίνει απευθείας σαν παράμετρο στη μέθοδο AddPayment().
-            System.out.println(cust.getBalance()); // ΓΙΑ ΕΛΕΓΧΟ ΣΩΣΤΗΣ ΛΕΙΤΟΥΡΓΙΑΣ
-            System.out.println(cust.getPayments(i)); // ΓΙΑ ΕΛΕΓΧΟ ΣΩΣΤΗΣ ΛΕΙΤΟΥΡΓΙΑΣ
+            if (cust.getBalance == 0) { // Έλεγχος αν μηδενίστηκε το υπόλοιπο του πελάτη.
+                System.out.print("Δώσε το ποσό πληρωμής[" + i + "] : ");
+                cust.AddPayment(UserInput.getDouble()); // Την εισαγωγή του χρήστη απ' το πληκτρολόγιο τη δίνει απευθείας σαν παράμετρο στη μέθοδο AddPayment().
+                System.out.println(cust.getBalance()); // ΓΙΑ ΕΛΕΓΧΟ ΣΩΣΤΗΣ ΛΕΙΤΟΥΡΓΙΑΣ
+                System.out.println(cust.getPayments(i)); // ΓΙΑ ΕΛΕΓΧΟ ΣΩΣΤΗΣ ΛΕΙΤΟΥΡΓΙΑΣ
+            }
+            else { // Αν μηδενίστηκε το υπόλοιπο του πελάτη εμφανίζει το κατάλληλο μήνυμα και "σπάει" το for.
+                System.out.println("Το υπόλοιπο του πελάτη μηδενίστηκε.");
+                return;
+            }
         }
 
         System.out.println(cust.toString()); // Εμφανίζει τα στοιχεία όλων των πληρωμών. Ζητούμενο της άσκησης.

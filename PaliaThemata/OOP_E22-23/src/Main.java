15c50a0874a8ef4808cc83025c21712fefc8eee2
panosale <62485609+panosale@users.noreply.github.com>
2023-07-07 17:23:38 +0300

    Update Main.java

--- PaliaThemata/OOP_E22-23/src/Main.java
+++ PaliaThemata/OOP_E22-23/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Customer cust = new Customer(1, 1000); // Δημιουργία αντικειμένου "πελάτη"
+        Customer cust = new Customer(1, 1000); // Δημιουργία αντικειμένου "πελάτη" με id:1 και υπόλοιπο 1000€. Ζητούμενο της άσκησης.
 /*      // ΓΙΑ ΔΟΚΙΜΕΣ
         cust.AddPayment(10);
         cust.AddPayment(100);
         cust.AddPayment(1000);
 */
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { // Καταχώρηση 10 πληρωμών. Ζητούμενο της άσκησης.
             System.out.print("Δώσε το ποσό πληρωμής[" + i + "] : ");
             cust.AddPayment(UserInput.getDouble());
             System.out.println(cust.getBalance()); // ΓΙΑ ΕΛΕΓΧΟ ΣΩΣΤΗΣ ΛΕΙΤΟΥΡΓΙΑΣ
